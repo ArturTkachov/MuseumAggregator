c615b6934accc9691eaff10ab59922bbcda5a427
ArturTkachov <arturtk.tkachov@gmail.com>
2021-12-09 22:23:59 +0300

    add MenuButton hover style change

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -5,17 +5,18 @@ const IconButton = React.memo(
   function (props) {
     return (props.submit) ?
       (
-        <button className="icon-button" type="submit">
+        <button className="icon-button" onPointerOver={props.handlePointerOver}
+                type="submit" onPointerLeave={props.handlePointerLeave}>
           <img className={props.iconClass || ""} src={props.iconSrc} alt="submit button"/>
         </button>
       )
       :
       (
-        <button className="icon-button" onClick={props.handleClick}>
+        <button className="icon-button" onClick={props.handleClick}
+                onPointerOver={props.handlePointerOver} onPointerLeave={props.handlePointerLeave}>
           <img className={props.iconClass || ""} src={props.iconSrc} alt="button"/>
         </button>
       );
-
   }
 );
 
--- src/components/header/MenuButton.js
+++ src/components/header/MenuButton.js
@@ -2,18 +2,42 @@ import React from 'react';
 import IconButton from '../IconButton';
 
 import menuIcon from '../../assets/icons/red/menuRed.svg';
+import menuWhiteIcon from '../../assets/icons/white/menuWhite.svg'
 import '../css/header/MenuButton.css'
 
 class MenuButton extends React.PureComponent{
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state= {
+      iconClass: "menu-button-icon",
+      iconSrc: menuIcon,
+    };
+
+    this.handlePointerOver = this.handlePointerOver.bind(this);
+    this.handlePointerLeave = this.handlePointerLeave.bind(this);
+  }
+
+  handlePointerOver(){
+    this.setState({
+      iconClass: "menu-button-icon-hover",
+      iconSrc: menuWhiteIcon,
+    });
+  }
+
+  handlePointerLeave(){
+    this.setState({
+      iconClass: "menu-button-icon",
+      iconSrc: menuIcon,
+    });
   }
 
   render() {
     return(
       <IconButton submit={false}
-                  iconClass="menu-button-icon"
-                  iconSrc={menuIcon}
+                  handlePointerOver={this.handlePointerOver}
+                  handlePointerLeave={this.handlePointerLeave}
+                  iconClass={this.state.iconClass}
+                  iconSrc={this.state.iconSrc}
       />
     );
   }
