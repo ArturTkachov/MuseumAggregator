89a804c809896796bfedd556be001aecd521cb8d
ArturTkachov <arturtk.tkachov@gmail.com>
2022-02-03 19:44:25 +0300

    change TryNowButton navigation path

--- src/components/home/TryNowButton.tsx
+++ src/components/home/TryNowButton.tsx
@@ -3,7 +3,7 @@ import RedButton from '../../components/RedButton';
 
 const TryNowButton = () => {
   const navigate = useNavigate();
-  const handleClick = () => navigate('collections');
+  const handleClick = () => navigate('../collections');
 
   return (<RedButton text="Try now!" size="big" handleClick={handleClick}/>);
 };
