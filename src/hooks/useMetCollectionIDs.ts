1e6e21bd7ab70aeebfbfcf52e00f638240ba73a1
ArturTkachov <arturtk.tkachov@gmail.com>
2022-02-21 15:07:29 +0300

    add useRandomSpecifiedCollectionIDs hook

--- src/hooks/useMetCollectionIDs.ts
+++ src/hooks/useMetCollectionIDs.ts
@@ -1,13 +1,13 @@
 import { useGetMetDepartmentArtworksIDsQuery } from '../apis/metApi';
 import { CollectionType } from '../types/CollectionType';
 
-const useMetCollectionIDs = (department: CollectionType): number[] | null => {
+const useMetCollectionIDs = (collection: CollectionType): number[] | null => {
   const metDepartmentIDs = {
     [CollectionType.Antiquity]: 13,
     [CollectionType.Asia]: 6,
     [CollectionType.MiddleAges]: 17,
   };
-  const depID = metDepartmentIDs[department];
+  const depID = metDepartmentIDs[collection];
   const { data: ids, isLoading } = useGetMetDepartmentArtworksIDsQuery(depID);
 
   if (isLoading || !ids) return null;
