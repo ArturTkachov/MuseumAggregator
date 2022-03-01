85fa0a5a61c255a776b3d6663f42d89190333ba1
ArturTkachov <arturtk.tkachov@gmail.com>
2022-03-01 17:20:48 +0300

    refactor MainSearchBar to be FC; add aria-labels

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,25 @@
-import React, {FC} from 'react';
-import './css/SearchBar.css'
+import React, { FC, ChangeEventHandler, FormEventHandler } from 'react';
+import './css/SearchBar.css';
 
-interface Props{
-  size: "small" | "normal",
-  value: string,
-  handleSubmit: () => void,
-  handleChange: () => void
+interface Props {
+  size: 'small' | 'normal';
+  value: string;
+  handleSubmit: FormEventHandler<HTMLFormElement>;
+  handleChange: ChangeEventHandler<HTMLInputElement>;
 }
 
-const SearchBar:FC<Props> = (props) => {
+const SearchBar: FC<Props> = (props) => {
   const formClass = `search-bar ${props.size}`;
 
   return (
     <form className={formClass} onSubmit={props.handleSubmit}>
-      <input type="text" value={props.value} onChange={props.handleChange}/>
-      <button/>
+      <input
+        type="text"
+        value={props.value}
+        onChange={props.handleChange}
+        aria-label="search input"
+      />
+      <button name="search button" aria-label="search button" />
     </form>
   );
 };
---
+++ src/components/header/MainSearchBar.tsx
@@ -0,0 +1,33 @@
+import React, {
+  FC,
+  useState,
+  ChangeEventHandler,
+  FormEventHandler,
+} from 'react';
+import SearchBar from '../SearchBar';
+import { useNavigate } from 'react-router-dom';
+
+const MainSearchBar = React.memo<FC>(() => {
+  const [value, setValue] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) =>
+    setValue(e.target.value);
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    setValue('');
+    navigate(`./search/${value}`);
+  };
+
+  return (
+    <SearchBar
+      size="normal"
+      value={value}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  );
+});
+
+export default MainSearchBar;
